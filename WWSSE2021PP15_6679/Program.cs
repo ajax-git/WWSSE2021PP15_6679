c6544137f68656c75e68581e41d13fd01ac4c20b
ajax-git <ajax.grandtheftauto@gmail.com>
2021-12-08 13:04:31 +0100

    Dodaj pliki projektów.

---
+++ WWSSE2021PP15_6679/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+    Napisz program, który wczytuje ze standardowego wejścia kolejne znaki i zamienia małe litery na duże i zapisuje na standardowe wyjście.
+*/
+
+// https://docs.microsoft.com/pl-pl/dotnet/standard/base-types/changing-case
+// ToUpper - na wielkie litery
+// ToLower - na małe litery
+
+Console.WriteLine("Wpisz jakiś tekst: ");
+string text = Console.ReadLine();
+
+Console.WriteLine(text.ToUpper());
